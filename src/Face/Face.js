f9b27c3ee8487703812e83fbcccf0c5fbe3c4ff7
Yevhenii <Muryi>
2019-08-30 22:09:54 +0300

    filters done

--- src/Face/Face.js
+++ src/Face/Face.js
@@ -5,11 +5,12 @@ import { Actions } from 'react-native-router-flux';
 import styles from './styles';
 import { getNote } from '../utils/AsyncStorage';
 
-function Face(props) {
-  const { note } = props;
+function Face() {
   const [isDialogVisible, setDialog] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
   const [notes, setNotes] = useState([]);
+  const [filteredNotes, setFiltered] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   const newNote = async (noteName) => {
     Actions.editor({
@@ -21,14 +22,36 @@ function Face(props) {
   useEffect(() => {
     getNote().then((response) => setNotes(JSON.parse(response)));
   }, []);
+
   useEffect(() => {
     setRefreshing(false);
+    setFiltered(notes);
   }, [notes]);
+
+  useEffect(() => {
+    let filtered;
+    switch (filter) {
+      case 'home':
+        filtered = notes.filter((item) => item.category === 'home');
+        break;
+      case 'work':
+        filtered = notes.filter((item) => item.category === 'work');
+        break;
+      case 'sport':
+        filtered = notes.filter((item) => item.category === 'sport');
+        break;
+      default:
+        filtered = notes;
+        break;
+    }
+    setFiltered(filtered);
+  }, [filter]);
+
 
   const renderNotes = () => (
     <View style={styles.listContainer}>
       <FlatList
-        data={notes}
+        data={filteredNotes}
         keyExtractor={(item, index) => `note${index}`}
         onEndReached={() => {}}
         onEndReachedThreshold={0.01}
@@ -36,44 +59,86 @@ function Face(props) {
         renderItem={({ item }) => renderNote(item)}
         initialNumToRender={10}
         maxToRenderPerBatch={10}
-        refreshing = {refreshing}
-        onRefresh = {() => {
+        refreshing={refreshing}
+        onRefresh={() => {
           setRefreshing(true);
           getNote().then((response) => setNotes(JSON.parse(response)));
-        }
-        }
+        }}
       />
     </View>
   );
 
+  const renderNewButton = () => (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => {
+        setDialog(true);
+        console.log(notes, 'lol');
+      }}
+    >
+      <Text>Create new note</Text>
+    </TouchableOpacity>
+  );
+
   const renderNote = (note) => (
-    <TouchableOpacity style={styles.note}>
+    <TouchableOpacity style={styles.note} onPress={() => {
+      Actions.editor({
+        note: { name: note.name, text: note.text, category: note.category },
+        title: `Edit note ${note.name}`
+      });
+    }}>
       <Text>{note.name}</Text>
     </TouchableOpacity>
   );
 
+  const renderFilters = () => (
+    <View style={styles.categoryContainer}>
+      <Text>Filter:</Text>
+      <TouchableOpacity
+        style={styles.category}
+        onPress={() => setFilter('all')}
+      >
+        <Text>All</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.category}
+        onPress={() => setFilter('home')}
+      >
+        <Text>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.category}
+        onPress={() => setFilter('work')}
+      >
+        <Text>Work</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.category}
+        onPress={() => setFilter('sport')}
+      >
+        <Text>Sport</Text>
+      </TouchableOpacity>
+    </View>
+  );
+  const renderModal = () => (
+    <DialogInput
+      isDialogVisible={isDialogVisible}
+      title={'New note'}
+      message={'Please name your note'}
+      hintInput={'name'}
+      submitInput={(inputText) => {
+        newNote(inputText).then(() => setDialog(false));
+      }}
+      closeDialog={() => setDialog(false)}
+    />
+  );
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          setDialog(true);
-          console.log(notes, 'lol');
-        }}
-      >
-        <Text>Create new note</Text>
-      </TouchableOpacity>
-        {renderNotes()}
-      <DialogInput
-        isDialogVisible={isDialogVisible}
-        title={'New note'}
-        message={'Please name your note'}
-        hintInput={'name'}
-        submitInput={(inputText) => {
-          newNote(inputText).then(() => setDialog(false));
-        }}
-        closeDialog={() => setDialog(false)}
-      />
+      {renderNewButton()}
+      {renderFilters()}
+      {renderNotes()}
+      {renderModal()}
     </View>
   );
 }
--- src/Face/styles.js
+++ src/Face/styles.js
@@ -20,5 +20,31 @@ export default StyleSheet.create({
   listContainer: {
     flex: 1,
     marginTop: 10
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10
+  },
+  category: {
+    height: 30,
+    width: 50,
+    backgroundColor: '#fff',
+    borderRadius: 16,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#E9E2F4',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  note: {
+    width: '100%',
+    height: 60,
+    borderWidth: 1,
+    borderColor: 'grey',
+    marginBottom: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFE18F'
   }
 });
